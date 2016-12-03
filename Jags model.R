1dd3b53f4bcd51945bb4668802d4800ad582b7e9
FibonacciGirl <isdestefano@vassar.edu>
2016-12-03 13:57:44 -0500

    W ~ Wishart

--- Jags model.R
+++ Jags model.R
@@ -1,4 +1,4 @@
-install.packages("runjags")
+install.packages("rjags")
 
 library(rjags)
 library(runjags)
@@ -9,13 +9,13 @@ category<-data$category
 data$category<-NULL
 n.categories<-length(unique(category))
 E<-t(data.matrix(data))
+dim(E)
 
-dim(Y)
 dim(L0)
 mu0= matrix(c(.5,.5),nrow=2,ncol=1)
 L0= matrix(c(0.4328403, 0.0406657, 0.0406657, 0.4222715),nrow=2,ncol=2)
-nu0= 4
+W0= matrix(c(0.4328403, 0.0406657, 0.0406657, 0.4222715),nrow=2,ncol=2)
 
 
-data.model<-list(E=E, S0=matrix(c(0.4328403, 0.0406657, 0.0406657, 0.4222715),nrow=2,ncol=2), L0=matrix(c(0.4328403, 0.0406657, 0.0406657, 0.4222715),nrow=2,ncol=2),mu0= matrix(c(.5,.5),nrow=2,ncol=1), n = dim(Y)[2], p = dim(Y)[1], m=n.categories)
+data.model<-list(E=E, S0=matrix(c(0.4328403, 0.0406657, 0.0406657, 0.4222715),nrow=2,ncol=2),W0=matrix(c(0.4328403, 0.0406657, 0.0406657, 0.4222715),nrow=2,ncol=2), L0=matrix(c(0.4328403, 0.0406657, 0.0406657, 0.4222715),nrow=2,ncol=2),mu0= matrix(c(.5,.5),nrow=2,ncol=1), n = dim(E)[2], p = dim(E)[1], m=n.categories)
 result<- run.jags('model.txt', data=data.model, monitor=c('theta','W','mu','sigma'), n.chains=3, burnin=1000, sample=10000)
