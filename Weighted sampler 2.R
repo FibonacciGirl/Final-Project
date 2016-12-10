beb3626b764e44806cc8ff35021b273457d31a53
FibonacciGirl <isdestefano@vassar.edu>
2016-12-09 20:18:33 -0500

    New stuff

--- Weighted sampler 2.R
+++ Weighted sampler 2.R
@@ -8,40 +8,20 @@ library(MCMCpack)
 library(coda)
 
 
-##credit Ravi Varadhan rvaradhan at jhmi.edu ####
-Posdef <- function (r, ev = runif(r, 0, 1)) 
-{
-  U <- matrix(ncol=r, rnorm(r^2))
-  decomp <- qr(U)
-  Q <- qr.Q(decomp) 
-  R <- qr.R(decomp)
-  d <- diag(R)
-  ph <- d / abs(d)
-  O <- Q %*% diag(ph)
-  H <- t(O) %*% diag(ev) %*% O
-  return(H)
-}  
-
-Posdef(2)
-######################
-
-
-
-
 #latent class stuff#
 
-MCMC<-function(Y){
+MCMC<-function(Y,category){
   
   n.categories<-length(unique(category))
-  n<-dim(Y)[2]
-  n.dimensions<-dim(Y)[1]
+  n<-dim(Y)[1]
+  n.dimensions<-dim(Y)[2]
   
   ybar<-array(dim= c(n.categories,n.dimensions))
   nj<-numeric()
   
   for(j in 1:n.categories){
-    ybar[j,]<-mean(Y[,which(category==j)])
-    nj[j]<-length(Y[,which(category==j)])
+    ybar[j,]<-mean(Y[which(category==j),])
+    nj[j]<-length(Y[which(category==j),])
   }
 
   
@@ -59,21 +39,28 @@ MCMC<-function(Y){
   
   
   #choose fixed hyper paratments#
-  mu0<-array(rep(0,n.dimensions*n.categories),dim=c(n.categories,n.dimensions))
+  mu0<-array(dim=c(n.categories,n.dimensions))
   Lambda0<-array(dim=c(n.categories,n.dimensions,n.dimensions))
   for(j in 1:n.categories){
+    mu0[j,]<-ybar[j,]
     Lambda0[j,,]<-rWishart(1,n.dimensions,diag(1,n.dimensions,n.dimensions))
   }
+  mu0
   Lambda0[1,,]
   
   
   nu0<-rep(n.dimensions,n.categories)
   sigma0<-rWishart(1,n.dimensions,diag(100,n.dimensions,n.dimensions))
   sigma0<-solve(matrix(sigma0,nrow=n.dimensions,ncol=n.dimensions))
-  
+
   W0<-array(dim=c(n.categories,n.dimensions,n.dimensions))
+  Whold<-array(dim=c(n.categories,n.dimensions*n.dimensions))
   for(j in 1:n.categories){
+    
     W0[j,,]<-rWishart(1,n.dimensions,diag(1,n.dimensions,n.dimensions))
+    Whold[j,]<-as.vector(nearPD(W0[j,,])$mat)
+    W0[j,,]<-matrix(Whold[j,],nrow=n.dimensions,ncol=n.dimensions)
+    
   }
   alpha<-rep(1,n.categories)
   
@@ -95,14 +82,14 @@ MCMC<-function(Y){
     Sigma[1,j,,]<-solve(matrix(Sigma.inv,nrow=n.dimensions,ncol=n.dimensions))
   }
   
-  ##begin sampler##
+################     begin sampler    ##################
   for(iter in 2:nrun){
     print(iter)
     witer<-array(dim=c(n.categories,n.dimensions,n.dimensions))
     wit<-array(dim=c(n.categories,n.dimensions*n.dimensions))
     for(j in 1:n.categories){
     wit[j,]<-as.vector(nearPD(W[(iter-1),j,,])$mat) 
-    witer[j,,]<-matrix(wit[j,],nrow=n.dimensions,ncol=n.dimensions)
+    witer[j,,]<-matrix(wit[j,],nrow=n.dimensions,ncol=n.dimensions)/sum(wit[j])
     }
     ##update Z and W##
     
@@ -114,7 +101,9 @@ MCMC<-function(Y){
     
     for(j in 1:n.categories){
       W.jump[j,,]<-rWishart(1,n.dimensions,witer[j,,])
+      W.jump[j,,]<-W.jump[j,,]/(W.jump[j,,])
     }
+   
     
     loglik.w.current<-numeric()
     loglik.w.jump<-numeric()
@@ -132,10 +121,21 @@ MCMC<-function(Y){
       WW.c<-as.vector(nearPD(witer[j.z.current,,]%*%Sigma[(iter-1),j.z.current,,]%*%t(witer[j.z.current,,]))$mat)
       WW.c<-matrix(WW.c,nrow= n.dimensions,ncol=n.dimensions)
       WW.j<-as.vector(nearPD(witer[j.z.jump,,]%*%Sigma[(iter-1),j.z.jump,,]%*%t(witer[j.z.jump,,]))$mat)
-      WW.j<-matrix(WW.j,nrow= n.dimensions,ncol=n.dimensions)         
+      WW.j<-matrix(WW.j,nrow= n.dimensions,ncol=n.dimensions)       
+      
+      E.z.c<-(witer[j.z.current,,]%*%Y[s,])
+      E.z.j<-(witer[j.z.jump,,]%*%Y[s,])
+      print(witer[j.z.current,,]%*%Mu[(iter-1),j.z.current,])
+      print(witer[j.z.jump,,]%*%Mu[(iter-1),j.z.jump,])
+      
+      print(length(witer[j.z.current,,]%*%Mu[(iter-1),j.z.current,]))
+      print(length(witer[j.z.jump,,]%*%Mu[(iter-1),j.z.jump,]))
+      print(WW.c)
+      print(WW.j)
+      
                                                                
-      loglik.z.current<-loglik.z.current + log(dmvnorm(Y[,s],witer[j.z.current,,]%*%Mu[(iter-1),j.z.current,],WW.c))
-      loglik.z.jump<-loglik.z.jump+log(dmvnorm(Y[,s],witer[j.z.jump,,]%*%Mu[(iter-1),j.z.jump,],WW.j))
+      loglik.z.current<-loglik.z.current + log(dmvnorm(E.z.c,witer[j.z.current,,]%*%Mu[(iter-1),j.z.current,],WW.c))
+      loglik.z.jump<-loglik.z.jump+log(dmvnorm(E.z.j,witer[j.z.jump,,]%*%Mu[(iter-1),j.z.jump,],WW.j))
       prior.z.current<-prior.z.current+log(dmultinom(Z[(iter-1),s,],1,theta[(iter-1),]))
       prior.z.jump<-prior.z.jump+log(dmultinom(Z.jump[s,],1,theta[(iter-1),]))
         
@@ -143,8 +143,12 @@ MCMC<-function(Y){
       ##get rid of repeat
       jump.w.sd<-as.vector(nearPD(W.jump[j.z.jump,,]%*%Sigma[(iter-1),j.z.current,,]%*%t(W.jump[j.z.current,,]))$mat)
       jump.w.sd<-matrix(jump.w.sd,nrow=n.dimensions,ncol=n.dimensions)                  
-      loglik.w.current<-loglik.w.current+log(dmvnorm(Y[,s],witer[j.z.current,,]%*%Mu[(iter-1),j.z.current,],WW.c))
-      loglik.w.jump<-loglik.w.jump +log(dmvnorm(Y[,s],W.jump[j.z.current,,]%*%Mu[(iter-1),j.z.current,],jump.w.sd))
+      E.w.c<-(witer[j.z.current,,]%*%Y[s,])
+      E.w.j<-(W.jump[j.z.current,,]%*%Y[s,])
+      
+      
+      loglik.w.current<-loglik.w.current+log(dmvnorm(E.w.c,witer[j.z.current,,]%*%Mu[(iter-1),j.z.current,],WW.c))
+      loglik.w.jump<-loglik.w.jump +log(dmvnorm(E.w.j,W.jump[j.z.current,,]%*%Mu[(iter-1),j.z.current,],jump.w.sd))
       prior.w.current<-prior.w.current+log(dwish(witer[j.z.current,,],n.dimensions,W0[j,,]))
       prior.w.jump<-prior.w.jump+log(dwish(W.jump[j.z.current,,],n.dimensions,W0[j,,]))
 
@@ -192,6 +196,7 @@ MCMC<-function(Y){
       Z.sum[j]<-length(which(z.j==j))
       alpha_n[j]<-alpha[j] +Z.sum[j]
     }
+    
     theta[iter,]<-rdirichlet(1,alpha_n)
     
     #print(theta[iter,])
@@ -203,10 +208,10 @@ MCMC<-function(Y){
       h<-matrix(h,nrow=n.dimensions,ncol=n.dimensions)
       A<-nj[j]*(t(witer[j,,])%*%solve(h)%*%witer[j,,]) + solve(Lambda0[j,,])
       A<-as.vector(nearPD(A)$mat)
-      A<-solve(matrix(A,nrow=n.dimensions,ncol=n.dimensions))
+      A.inv<-solve(matrix(A,nrow=n.dimensions,ncol=n.dimensions))
 
-      b<-solve(Lambda0[j,,])%*%mu0[j,] + nj[j]*t(witer[j,,])%*%solve(h)%*%ybar[j,]
-      Mu[iter,j,]<-rmvnorm(1,A%*%b,A)
+      b<-solve(Lambda0[j,,])%*%mu0[j,] + nj[j]*t(witer[j,,])%*%solve(h)%*%witer[j,,]%*%ybar[j,]
+      Mu[iter,j,]<-rmvnorm(1,A.inv%*%b,A.inv)
     }
 
     #print('################   Mu updated   ################')
@@ -219,7 +224,7 @@ MCMC<-function(Y){
     sum.sig<-array(sigma0,dim=c(n.dimensions,n.dimensions))
     for(i in 1:n){
       if(category[i]==j){
-        sum.sig<- sum.sig + solve(witer[j,,])%*%(Y[,i]-witer[j,,]%*%Mu[(iter-1),j,])%*%t(Y[,i]-witer[j,,]%*%Mu[(iter-1),j,])%*%solve(t(witer[j,,]))
+        sum.sig<- sum.sig + solve(witer[j,,])%*%(witer[j,,]%*%Y[i,]-witer[j,,]%*%Mu[(iter-1),j,])%*%t(witer[j,,]%*%Y[i,]-witer[j,,]%*%Mu[(iter-1),j,])%*%solve(t(witer[j,,]))
       }
     }
 
@@ -228,11 +233,9 @@ MCMC<-function(Y){
 
 
       sig.inv<-rWishart(1,nun,Sw[j,,])
-      print(sig.inv)
       Sigma[iter,j,,]<-solve(matrix(sig.inv,nrow=n.dimensions,ncol=n.dimensions))
 
   }
-    print(Sigma[iter,,,])
 
     #print('################   Sigma updated   ################')
     
@@ -246,48 +249,38 @@ MCMC<-function(Y){
       W.out[sav,,,]<-W[iter,,,]
     }
   }
-  #return(theta,Sigma, Mu,Z)
-  return(list(THETA=Theta.out,SIGMA=Sigma.out,MU=Mu.out,Z.OUT=Z.out,W.OUT=W.out))
+  return(list(theta,Sigma, Mu,Z,W))
+  ##return(list(THETA=Theta.out,SIGMA=Sigma.out,MU=Mu.out,Z.OUT=Z.out,W.OUT=W.out))
 }
 
 
 
 
 ##data##
-data<-read.csv('TD10',header=TRUE)
+data<-training.data
+data
 category<-data$category
 data$category<-NULL
 x<-data$x
 y<-data$y
 
-Y<-t(cbind(as.matrix(x),as.matrix(y)))
+Y<-cbind(as.matrix(x),as.matrix(y))
 
 n.categories<-length(unique(category))
-n<-dim(Y)[2]
-n.dimensions<-dim(Y)[1]
+n<-dim(Y)[1]
+n.dimensions<-dim(Y)[2]
+
+print(n.categories)
 
 ybar<-array(dim= c(n.categories,n.dimensions))
 nj<-numeric()
 
 for(j in 1:n.categories){
-  ybar[j,]<-mean(Y[,which(category==j)])
-  nj[j]<-length(Y[,which(category==j)])
+  ybar[j,]<-mean(Y[which(category==j),])
+  nj[j]<-length(Y[which(category==j),])
 }
+print(Y[which(category==1),])
+Y
 
+test<- MCMC(Y,category)
 
-test<- MCMC(Y)
-
-
-th<-test[[1]]
-si<-test[[2]]
-mu<-test[[3]]
-z.z<-test[[4]]
-w.w<-test[[5]]
-
-density(mu)
-
-
-plot(density(w.w[,2,2,1]))
-plot(density(z.z[,1,]))
-sigma<-as.mcmc.list(sigma1.mcmc,sigma2.mcmc)
-traceplot(sigma1.mcmc)
